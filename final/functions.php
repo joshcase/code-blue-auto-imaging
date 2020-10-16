e6a035375f84ad0c875b80f8fbcab22d0afd20ae
JoshCase <joshtcase@gmail.com>
2020-10-16 18:37:30 +1000

    Remove dummy MailTrap username and password

--- final/functions.php
+++ final/functions.php
@@ -69,8 +69,8 @@
 	    $mail->isSMTP();                                            // Send using SMTP
 	    $mail->Host       = 'smtp.mailtrap.io';                    // Set the SMTP server to send through
 	    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-	    $mail->Username   = 'b983e67cd93c5f';                     // SMTP username
-	    $mail->Password   = 'a0b01b409a8a39';                               // SMTP password
+	    $mail->Username   = 'your-mailtrap-username';                     // SMTP username
+	    $mail->Password   = 'your-mailtrap-password';                               // SMTP password
 	    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
